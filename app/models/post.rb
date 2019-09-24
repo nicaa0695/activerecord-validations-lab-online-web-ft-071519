de7dcce0639ebef74da86ee1445b2e84f63e15b0
Veronica Lopez <nicaa0695@gmail.com>
2019-09-24 19:00:07 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,14 @@
-
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\b(Won't Believe|Secret|Top|Guess)\b/i
+      record.errors[attribute] << (options[:message] || "is not clickbait")
+      end
+  end
+end
 
 class Post < ActiveRecord::Base
   validates :title, presence: true, title: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  
-    
 end
