109a366892f381636ee19853179e9e9b49229403
Veronica Lopez <nicaa0695@gmail.com>
2019-09-24 18:58:08 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ActiveRecord::Base
-   def validate_each(record, attribute, value)
-        unless value =~ /\b(Won't Believe|Secret|Top|Guess)\b/i
-            record.errors[attribute] << (options[:message] || "is not clickbait")
-        end
-    end
+  
+  
     
 end
